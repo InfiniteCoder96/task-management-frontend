e395f29b2267ac08367c1851b688b477ca861a80
InfiniteCoder96 <InfiniteCoder96@users.noreply.github.com>
2021-07-12 08:02:52 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.fb2f3be51c6840c2045417ee952823b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e97c239617652d2197628cf8582c1c",
+    "url": "https://infinitecoder96.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "3ef30abf44cd4dab8a64",
+    "url": "https://infinitecoder96.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "6472f618c02b46436fdd",
+    "url": "https://infinitecoder96.github.io/task-management-frontend/static/js/2.bc283443.chunk.js"
+  },
+  {
+    "revision": "3ef30abf44cd4dab8a64",
+    "url": "https://infinitecoder96.github.io/task-management-frontend/static/js/main.183eaa7e.chunk.js"
+  },
+  {
+    "revision": "15889ae9b3b7b17ac8ac",
+    "url": "https://infinitecoder96.github.io/task-management-frontend/static/js/runtime~main.7a05cccd.js"
+  }
+]);
